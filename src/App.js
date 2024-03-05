5048079b94969f39d7093e6c86ee160f2d7a7ba6
DianaShut <146943634+DianaShut@users.noreply.github.com>
2024-03-05 21:01:13 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import './App.css';
+import {useState} from "react";
+import {ComponentOne} from "./components/ComponentOne";
+import {ComponentTwo} from "./components/ComponentTwo";
+import {useSelector} from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const{count} = useSelector(state => state.counter1) //state - це store, з якого витягуємо reducer і цей хук віддає initialState нашого слайсу
+    const {count:count2} = useSelector(state => state.counter2)
+    return (
+        <div>
+            <div>
+                count: {count}
+                <ComponentOne/>
+            </div>
+            <div>
+                count2: {count2}
+                <ComponentTwo/>
+            </div>
 
-export default App;
+        </div>
+    );
+};
+
+export {App};
